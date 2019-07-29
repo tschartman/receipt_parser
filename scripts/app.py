4eb34275dc3119f12806318bf7f872a89a0839e7
Tim Schartman <tschartman@yahoo.com>
2019-07-28 22:01:04 -0500

    adjusted finder to get all upc codes

--- scripts/app.py
+++ scripts/app.py
@@ -10,28 +10,23 @@ import json
 import re
 from bs4 import BeautifulSoup
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Simple image to string
-receiptArr = pytesseract.image_to_string(Image.open('test2.jpg')).split('\n')
+receiptArr = pytesseract.image_to_string(Image.open('test2.jpg'))
 
-start = receiptArr.index('CAULIPOWER 086287100032 F 6.48 Y')
+barcode = re.findall(r"[0-9]{12}", receiptArr)    
+print(barcode)
+for code in barcode:
+    url = 'https://www.walmart.com/search/?query=' + code
+    response = requests.get(url)
 
-for index, line in enumerate(receiptArr):
-        if(index >= start and index <= start + 3 ):  
-            barcode = re.findall(r"[0-9]{12}", line)[0]     
-            url = 'https://www.walmart.com/search/?query=' + barcode
-            response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-            soup = BeautifulSoup(response.text, "html.parser")
+    data = json.loads(soup.find(id='searchContent').string)
 
-            data = json.loads(soup.find(id='searchContent').string)
-
-            try:
-                title = BeautifulSoup(data['searchContent']['preso']['items'][0]['title'], "html.parser").get_text()
-                description = BeautifulSoup(data['searchContent']['preso']['items'][0]['description'], "html.parser").get_text()
-                print("Title: ", title)
-                print("Description: ", description)
-                print('\n')
-            except IndexError:
-                print("not found")
+    try:
+        title = BeautifulSoup(data['searchContent']['preso']['items'][0]['title'], "html.parser").get_text()
+        print(title)
+    except IndexError:
+        print("not found")
