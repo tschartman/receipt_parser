06e7e6954ea60cc5e1d66c9d1d9949caef4798cc
Tim Schartman <tschartman@yahoo.com>
2019-07-29 22:05:02 -0500

    added ability to find # of items bought

--- scripts/app.py
+++ scripts/app.py
@@ -13,12 +13,18 @@ from bs4 import BeautifulSoup
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Simple image to string
-receiptArr = pytesseract.image_to_string(Image.open('test2.jpg'))
+receiptArr = pytesseract.image_to_string(Image.open('test3.jpg'))
+#find the index of the amount of items (should be direclty after the word SOLD)
+index = receiptArr.split(' ').index("SOLD") + 1
+coupons = len(re.findall(r"COUPON", receiptArr))
+#try to parse the amount of items as an int
+#coupons are shown directly under so include them 
+items = int(re.findall(r"[0-9]*", receiptArr.split(' ')[index])[0]) + coupons
 
-barcode = re.findall(r"[0-9]{12}", receiptArr)    
-print(barcode)
-for code in barcode:
-    url = 'https://www.walmart.com/search/?query=' + code
+
+barcodeArr = re.findall(r"[0-9]{12}", receiptArr)    
+for i in range(0, items):
+    url = 'https://www.walmart.com/search/?query=' + barcodeArr[i]
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
